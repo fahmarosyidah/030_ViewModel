15ccf8193b4892cfc755180e26b1809ef86b4320
fahmarosyidah <fahmarosyi@gmail.com>
2023-10-23 18:05:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/registrasi/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.registrasi.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/registrasi/Data/DataSource.kt
@@ -0,0 +1,9 @@
+package com.example.registrasi.Data
+import com.example.registrasi.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
